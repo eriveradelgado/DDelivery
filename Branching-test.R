a2a96ddddcfaba36df1dd454ba066f74fcd15d93
eriveradelgado <edgardo.rivera@case.edu>
2016-07-06 14:54:19 -0400

    DDelivery
      Commit from Windows onto Chapter 2 branch

--- Branching-test.R
+++ Branching-test.R
@@ -5,6 +5,18 @@
 
 
 # Chapter 2 : This is a diff from Windows
+    # I created a branch in github and named it chapter two whatever
+    # modifications I do on this chapter are created from this computer.
+    # I already made sure to connect my different computers and verified
+    # I was able to push, pull and clone from github to my different
+    # devices. The problem I faced was switching between the shell
+    # and Rstudio Git pane. Things that can be a headeache so far
+    # is being in the wrong working directory, or in the incorrect branch.
+    # a good workflow I found from the following website:
+    # I'm looking forward to start collaborating with my students by 
+    # this mean to analyze our data and make updates to our workflow in a 
+    # distributed fashion. Hopefully this will make our research more robust.
+
 
 
 # Chapter 3 : This is a dif from EC2
